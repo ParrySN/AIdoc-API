1db67bf4f4f0bc64ff9fee264066d7436855345c
James T Jaisert <89368503+jamess66@users.noreply.github.com>
2024-12-04 15:31:51 +0700

    add swagger

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
 from report import report_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
 # from products import products_bp
 
 app = Flask(__name__)
+CORS(app) 
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "AIDOC API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 # Register Blueprints
 app.register_blueprint(report_bp, url_prefix='/')
 # app.register_blueprint(users_bp, url_prefix='/')
 # app.register_blueprint(products_bp, url_prefix='/api')
+
+
+
+app.json.ensure_ascii=False
 
 if __name__ == '__main__':
     app.run(debug=True)
