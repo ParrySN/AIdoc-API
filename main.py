7e23adcd43cdd5f386c8fafa250edcd5f53873c6
Nuttadon <nuttadon_p@cmu.ac.th>
2024-12-16 02:15:14 +0700

    feature report rework db to match with port 85

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, jsonify
-from admin import admin_bp
 from report import report_bp
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
@@ -27,8 +26,6 @@ app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 # Register Blueprints
 app.register_blueprint(report_bp, url_prefix='/')
-app.register_blueprint(admin_bp, url_prefix='/')
-
 
 app.json.ensure_ascii=False
 
